43bf1bc246d27a9d8d2009a91724dd96899e59e2
Thomas VELU <106349820+tvelu77@users.noreply.github.com>
2024-10-28 17:51:01 +0100

    add : form for creating a git

--- frontend/src/app/git-add/git-add.component.ts
+++ frontend/src/app/git-add/git-add.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { GitService } from '../git/git.service';
 import { Git } from '../git/git';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-git-add',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, FormsModule, ButtonModule],
   templateUrl: './git-add.component.html',
   styleUrl: './git-add.component.scss'
 })
 export class GitAddComponent {
   gitAddForm = new FormGroup({
     url: new FormControl('', Validators.required),
+    name: new FormControl('', Validators.required),
   });
 
   constructor(private readonly gitService: GitService) {}
@@ -20,7 +22,7 @@ export class GitAddComponent {
   onSubmit() {
     const newGit: Git = {
       url: this.gitAddForm.value.url!,
-      name: 'test'
+      name: this.gitAddForm.value.name!
     }
     this.gitService.add(newGit).subscribe(git => {
       console.log("hey");
