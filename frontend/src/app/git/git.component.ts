ca4d417256271bd871a90852b39dabdd1db61757
Thomas VELU <106349820+tvelu77@users.noreply.github.com>
2024-10-29 16:56:49 +0100

    feat : detailed page about a git

--- frontend/src/app/git/git.component.ts
+++ frontend/src/app/git/git.component.ts
@@ -1,20 +1,40 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { Git } from './git';
 import { GitService } from './git.service';
+import { MenuItem } from 'primeng/api';
+import { TabViewChangeEvent, TabViewModule } from 'primeng/tabview';
+import { DatePipe } from '@angular/common';
+import { AccordionModule } from 'primeng/accordion';
 
 @Component({
   selector: 'app-git',
   standalone: true,
-  imports: [],
+  imports: [TabViewModule, DatePipe, AccordionModule, RouterLink, RouterLinkActive],
   templateUrl: './git.component.html',
   styleUrl: './git.component.scss'
 })
 export class GitComponent {
   git!: Git;
   loading = true;
-  constructor(private readonly route: ActivatedRoute, private readonly gitService: GitService) {
-
+  items!: MenuItem[];
+  activeItem!: MenuItem;
+  constructor(private readonly route: ActivatedRoute, private readonly gitService: GitService, private readonly router: Router) {
+    this.items = [
+      {
+        label: 'Overview',
+        icon: 'pi pi-eye'
+      },
+      {
+        label: 'Metrics',
+        icon: 'pi pi-chart-pie'
+      },
+      {
+        label: 'Manage',
+        icon: 'pi pi-cog'
+      }
+    ];
+    this.activeItem = this.items[0];
   }
 
   ngOnInit() {
@@ -27,5 +47,13 @@ export class GitComponent {
       this.loading = false;
     });
   }
+
+  handleChange(event: TabViewChangeEvent) {
+    if (event.index === 3) {
+      this.router.navigate(['']);
+    }
+  }
+
+
 
 }
