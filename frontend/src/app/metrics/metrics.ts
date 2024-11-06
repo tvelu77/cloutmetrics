27207301972a069d8f8298facd8d2ecac18de00a
Thomas VELU <106349820+tvelu77@users.noreply.github.com>
2024-11-06 11:34:41 +0100

    add : git status and some metrics

--- frontend/src/app/metrics/metrics.ts
+++ frontend/src/app/metrics/metrics.ts
@@ -3,5 +3,9 @@ import { LanguagesRatio } from "./languages-ratio";
 export interface Metrics {
     id: number;
     totalCommits: number;
+    totalTags: number;
+    totalBranches: string;
+    owner: string;
     languageAndFileCount: LanguagesRatio;
+    languagesRatio: LanguagesRatio;
 }
