8af6e022a3a4a8a8063662ef8940e3a9705a54a4
Thomas VELU <106349820+tvelu77@users.noreply.github.com>
2024-11-26 11:47:59 +0100

    fix: before rejecting the deletion/rename operation, we check if the files exist

--- src/main/java/io/tvelu77/cloutmetrics/git/GitService.java
+++ src/main/java/io/tvelu77/cloutmetrics/git/GitService.java
@@ -2,8 +2,6 @@ package io.tvelu77.cloutmetrics.git;
 
 import io.tvelu77.cloutmetrics.ApplicationService;
 import io.tvelu77.cloutmetrics.Utils;
-import io.tvelu77.cloutmetrics.metrics.Metrics;
-import io.tvelu77.cloutmetrics.metrics.MetricsOperations;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -13,8 +11,6 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Objects;
-import java.util.concurrent.Executors;
-import org.eclipse.jgit.api.errors.GitAPIException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,46 +22,40 @@ public class GitService implements ApplicationService<Git> {
 
   @Autowired
   private GitRepository gitRepository;
-  
+
   @Autowired
   private Utils utils;
 
   @Override
   public boolean add(Git git) {
+    // TODO : DTO should be used here !
     Objects.requireNonNull(git);
-    var toBeSaved = new Git();
-    toBeSaved.setName(git.getName());
-    toBeSaved.setUrl(git.getUrl());
-    var metrics = new Metrics();
-    metrics.setTotalCommits(0L);
-    toBeSaved.setMetrics(metrics);
-    toBeSaved.setStatus(GitStatus.IN_PROGRESS);
-    gitRepository.save(toBeSaved);
-    var executor = Executors.newSingleThreadExecutor();
-    executor.submit(() -> {
-      try {
-        var operations = new MetricsOperations(git,
-                Path.of(utils.getLocalRepositoryPath() + git.getName()));
-        operations.openRepository();
-        metrics.setOwner(operations.getGitOwner());
-        gitRepository.save(toBeSaved);
-        metrics.setTotalCommits(operations.countCommits());
-        gitRepository.save(toBeSaved);
-        metrics.setTotalTags(operations.countTags());
-        gitRepository.save(toBeSaved);
-        metrics.setTotalBranches(operations.countBranches());
-        gitRepository.save(toBeSaved);
-        metrics.setLanguageAndFileCount(operations.countFilesForEachExtension());
-        gitRepository.save(toBeSaved);
-        metrics.setLanguageRatio(operations.languageRatio());
-        toBeSaved.setStatus(GitStatus.FINISHED);
-        gitRepository.save(toBeSaved);
-        operations.closeRepository();
-      } catch (GitAPIException | IOException e) {
-        toBeSaved.setStatus(GitStatus.ERROR);
-        gitRepository.save(toBeSaved);
-      }
-    });
+    gitRepository.save(git);
+    // var executor = Executors.newSingleThreadExecutor();
+    // executor.submit(() -> {
+    // try {
+    // var operations = new MetricsOperations(git,
+    // Path.of(utils.getLocalRepositoryPath() + git.getName()));
+    // operations.openRepository();
+    // metrics.setOwner(operations.getGitOwner());
+    // gitRepository.save(toBeSaved);
+    // metrics.setTotalCommits(operations.countCommits());
+    // gitRepository.save(toBeSaved);
+    // metrics.setTotalTags(operations.countTags());
+    // gitRepository.save(toBeSaved);
+    // metrics.setTotalBranches(operations.countBranches());
+    // gitRepository.save(toBeSaved);
+    // metrics.setLanguageAndFileCount(operations.countFilesForEachExtension());
+    // gitRepository.save(toBeSaved);
+    // metrics.setLanguageRatio(operations.languageRatio());
+    // toBeSaved.setStatus(GitStatus.FINISHED);
+    // gitRepository.save(toBeSaved);
+    // operations.closeRepository();
+    // } catch (GitAPIException | IOException e) {
+    // toBeSaved.setStatus(GitStatus.ERROR);
+    // gitRepository.save(toBeSaved);
+    // }
+    // });
     return true;
   }
 
@@ -74,24 +64,26 @@ public class GitService implements ApplicationService<Git> {
     Objects.requireNonNull(id);
     var git = gitRepository.findById(id).orElseThrow(() -> new NoSuchElementException());
     try {
-      if (git.getStatus() == GitStatus.FINISHED || git.getStatus() == GitStatus.ERROR) {
+      if (git.getStatus() != GitStatus.IN_PROGRESS) {
         deleteDirectory(Path.of(utils.getLocalRepositoryPath() + git.getName()));
         gitRepository.delete(git);
         return true;
       }
+      System.out.println("HELLO");
       return false;
     } catch (IOException e) {
+      System.out.println("HELLO BUG");
       return false;
     }
   }
 
-  @Override 
+  @Override
   public boolean update(Git newGit, Long id) {
     Objects.requireNonNull(newGit);
     Objects.requireNonNull(id);
     var git = gitRepository.findById(id).orElseThrow(() -> new NoSuchElementException());
     try {
-      if (git.getStatus() == GitStatus.FINISHED) {
+      if (git.getStatus() != GitStatus.IN_PROGRESS) {
         renameDirectory(Path.of(utils.getLocalRepositoryPath() + git.getName()),
             Path.of(utils.getLocalRepositoryPath() + newGit.getName()));
         git.setName(newGit.getName());
@@ -113,18 +105,28 @@ public class GitService implements ApplicationService<Git> {
   public Git findById(Long id) {
     return gitRepository.findById(id).orElseThrow(NoSuchElementException::new);
   }
-  
+
   private void deleteDirectory(Path directory) throws IOException {
     try (var paths = Files.walk(directory)) {
       paths.sorted(Comparator.reverseOrder()).map(Path::toFile).forEach(File::delete);
+    } catch (IOException e) {
+      if (Files.exists(directory)) {
+        throw e;
+      }
     }
   }
-  
+
   private void renameDirectory(Path directory, Path newDirectory) throws IOException {
-    Files.move(directory,
-        newDirectory,
-        StandardCopyOption.REPLACE_EXISTING,
-        StandardCopyOption.ATOMIC_MOVE);   
+    try {
+      Files.move(directory,
+          newDirectory,
+          StandardCopyOption.REPLACE_EXISTING,
+          StandardCopyOption.ATOMIC_MOVE);
+    } catch (IOException e) {
+      if (Files.exists(directory)) {
+        throw e;
+      }
+    }
   }
-  
+
 }
