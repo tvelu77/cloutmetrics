8899941ec4ec398b10a6a0ab0d1f2c2e484e7db7
Thomas VELU <106349820+tvelu77@users.noreply.github.com>
2024-11-26 16:04:23 +0100

    refacto: static import for better readability

--- src/test/java/io/tvelu77/cloutmetrics/git/GitControllerTest.java
+++ src/test/java/io/tvelu77/cloutmetrics/git/GitControllerTest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import java.io.IOException;
 import org.hamcrest.CoreMatchers;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -12,8 +13,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -24,70 +31,94 @@ public class GitControllerTest {
   private MockMvc mvc;
 
   @Test
-  public void emptyGetAll() throws Exception {
-    mvc.perform(MockMvcRequestBuilders.get("/gits")).andExpect(MockMvcResultMatchers.status().isOk())
-        .andExpect(MockMvcResultMatchers.content().json("[]"));
+  public void shouldReturnEmptyJsonAfterGet() throws Exception {
+    mvc.perform(get("/gits")).andExpect(status().isOk())
+            .andExpect(content().json("[]"));
   }
 
   @Test
-  public void returnErrorWithBadId() throws Exception {
-    mvc.perform(MockMvcRequestBuilders.get("/gits/100")).andExpect(MockMvcResultMatchers.status().is4xxClientError());
-  }
-
-  @Test
-  public void correctGet() throws Exception {
+  public void shouldReturnJsonAfterGet() throws Exception {
     var git = new Git(1L, "clout", "localhost");
-    mvc.perform(MockMvcRequestBuilders.post("/gits").contentType(MediaType.APPLICATION_JSON).content(toJson(git)))
-        .andExpect(MockMvcResultMatchers.status().isCreated())
-        .andExpect(MockMvcResultMatchers.jsonPath("name", CoreMatchers.is(git.getName())));
-    mvc.perform(MockMvcRequestBuilders.get("/gits/1")).andExpect(MockMvcResultMatchers.status().isOk())
-        .andExpect(MockMvcResultMatchers.jsonPath("name", CoreMatchers.is("clout")));
-  }
-  
-  @Test
-  public void badGet() throws Exception {
-    mvc.perform(MockMvcRequestBuilders.get("/gits/100")).andExpect(MockMvcResultMatchers.status().isNotFound());
+    var git2 = new Git(2L, "clout2", "localhost");
+    var git3 = new Git(3L, "clout3", "localhost");
+    mvc.perform(post("/gits").contentType(MediaType.APPLICATION_JSON).content(toJson(git)))
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("name", CoreMatchers.is(git.getName())));
+    mvc.perform(post("/gits").contentType(MediaType.APPLICATION_JSON).content(toJson(git2)))
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("name", CoreMatchers.is(git2.getName())));
+    mvc.perform(post("/gits").contentType(MediaType.APPLICATION_JSON).content(toJson(git3)))
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("name", CoreMatchers.is(git3.getName())));
+    mvc.perform(get("/gits")).andExpect(status().isOk())
+            .andExpect(jsonPath("$", Matchers.hasSize(3)));
   }
 
   @Test
-  public void correctPost() throws Exception {
+  public void shouldReturnGitAfterGetById() throws Exception {
     var git = new Git(1L, "clout", "localhost");
-    mvc.perform(MockMvcRequestBuilders.post("/gits").contentType(MediaType.APPLICATION_JSON).content(toJson(git)))
-        .andExpect(MockMvcResultMatchers.status().isCreated())
-        .andExpect(MockMvcResultMatchers.jsonPath("name", CoreMatchers.is(git.getName())));
+    mvc.perform(post("/gits").contentType(MediaType.APPLICATION_JSON).content(toJson(git)))
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("name", CoreMatchers.is(git.getName())));
+    mvc.perform(get("/gits/1")).andExpect(status().isOk())
+            .andExpect(jsonPath("name", CoreMatchers.is("clout")));
   }
 
   @Test
-  public void correctDelete() throws Exception {
+  public void shouldReturn404AfterGetByIdIfWrongId() throws Exception {
+    mvc.perform(get("/gits/100")).andExpect(status().isNotFound());
+  }
+
+  @Test
+  public void shouldReturn201AfterGetById() throws Exception {
     var git = new Git(1L, "clout", "localhost");
-    mvc.perform(MockMvcRequestBuilders.post("/gits").contentType(MediaType.APPLICATION_JSON).content(toJson(git)))
-        .andExpect(MockMvcResultMatchers.status().isCreated())
-        .andExpect(MockMvcResultMatchers.jsonPath("name", CoreMatchers.is(git.getName())));
-    mvc.perform(MockMvcRequestBuilders.delete("/gits/1")).andExpect(MockMvcResultMatchers.status().isNoContent());
-    mvc.perform(MockMvcRequestBuilders.get("/gits")).andExpect(MockMvcResultMatchers.content().json("[]"));
+    mvc.perform(post("/gits").contentType(MediaType.APPLICATION_JSON).content(toJson(git)))
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("name", CoreMatchers.is(git.getName())));
   }
 
   @Test
-  public void badDelete() throws Exception {
+  public void shouldReturn204AfterDelete() throws Exception {
+    var git = new Git(1L, "clout", "localhost");
+    mvc.perform(post("/gits").contentType(MediaType.APPLICATION_JSON).content(toJson(git)))
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("name", CoreMatchers.is(git.getName())));
+    mvc.perform(delete("/gits/1")).andExpect(status().isNoContent());
+    mvc.perform(get("/gits")).andExpect(content().json("[]"));
+  }
+
+  @Test
+  public void shouldReturn401AfterDeleteIfGitIsInProgress() throws Exception {
     var git = new Git(1L, "clout", "localhost");
     git.setStatus(GitStatus.IN_PROGRESS);
-    mvc.perform(MockMvcRequestBuilders.post("/gits").contentType(MediaType.APPLICATION_JSON).content(toJson(git)))
-        .andExpect(MockMvcResultMatchers.status().isCreated())
-        .andExpect(MockMvcResultMatchers.jsonPath("name", CoreMatchers.is(git.getName())));
-    mvc.perform(MockMvcRequestBuilders.delete("/gits/1")).andExpect(MockMvcResultMatchers.status().isUnauthorized());
+    mvc.perform(post("/gits").contentType(MediaType.APPLICATION_JSON).content(toJson(git)))
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("name", CoreMatchers.is(git.getName())));
+    mvc.perform(delete("/gits/1")).andExpect(status().isUnauthorized());
   }
 
   @Test
-  public void correctUpdate() throws Exception {
+  public void shouldReturn204AfterPut() throws Exception {
     var git = new Git(1L, "clout", "localhost");
-    mvc.perform(MockMvcRequestBuilders.post("/gits").contentType(MediaType.APPLICATION_JSON).content(toJson(git)))
-        .andExpect(MockMvcResultMatchers.status().isCreated())
-        .andExpect(MockMvcResultMatchers.jsonPath("name", CoreMatchers.is(git.getName())));
+    mvc.perform(post("/gits").contentType(MediaType.APPLICATION_JSON).content(toJson(git)))
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("name", CoreMatchers.is(git.getName())));
     git.setName("not clout");
-    mvc.perform(MockMvcRequestBuilders.put("/gits/1").contentType(MediaType.APPLICATION_JSON).content(toJson(git)))
-        .andExpect(MockMvcResultMatchers.status().isNoContent());
-    mvc.perform(MockMvcRequestBuilders.get("/gits/1")).andExpect(MockMvcResultMatchers.status().isOk())
-        .andExpect(MockMvcResultMatchers.jsonPath("name", CoreMatchers.is("not clout")));
+    mvc.perform(put("/gits/1").contentType(MediaType.APPLICATION_JSON).content(toJson(git)))
+            .andExpect(status().isNoContent());
+    mvc.perform(get("/gits/1")).andExpect(status().isOk())
+            .andExpect(jsonPath("name", CoreMatchers.is("not clout")));
+  }
+
+  @Test
+  public void shouldReturn404AfterPutIfBadId() throws Exception {
+    var git = new Git(1L, "clout", "localhost");
+    mvc.perform(post("/gits").contentType(MediaType.APPLICATION_JSON).content(toJson(git)))
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("name", CoreMatchers.is(git.getName())));
+    git.setName("hello");
+    mvc.perform(put("/gits/500").contentType(MediaType.APPLICATION_JSON).content(toJson(git)))
+            .andExpect(status().isNotFound());
   }
 
   private static byte[] toJson(Object object) throws IOException {
