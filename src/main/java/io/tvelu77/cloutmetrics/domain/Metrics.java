a0fd19c3c788ef459b581ec2c0211b6e36a1733e
Thomas VELU <106349820+tvelu77@users.noreply.github.com>
2024-12-09 12:04:04 +0100

    add: documentation

--- src/main/java/io/tvelu77/cloutmetrics/domain/Metrics.java
+++ src/main/java/io/tvelu77/cloutmetrics/domain/Metrics.java
@@ -12,9 +12,9 @@ import java.util.Map;
 import java.util.Objects;
 
 /**
- * Represents a git metrics :
- * - The number of commits ;
- * - The languages ratio ;
+ * Represents a {@link Git} metrics such as :<br>
+ * - The number of commits ;<br>
+ * - The languages ratio ;<br>
  * - etc (TODO : add more metrics such as the top list of contributor !).
  */
 @Entity
@@ -46,69 +46,149 @@ public class Metrics {
   @MapKeyColumn(name = "language")
   private Map<String, Double> languageRatio;
   
-  @Column
-  private Status status;
-  
-  private enum Status {
-    NOT_LAUNCHED,
-    STARTED,
-    IN_PROGRESS,
-    FINISHED,
-    ERROR
-  }
-  
+  /**
+   * Returns the id.
+   *
+   * @return {@link Long}, the metrics id.
+   */
   public Long getId() {
     return id;
   }
 
+  /**
+   * Sets the id.<br>
+   * The id should not be null nor inferior to 1.
+   *
+   * @param id {@link Long}, the new id.
+   */
   public void setId(Long id) {
-    this.id = Objects.requireNonNull(id);
+    Objects.requireNonNull(id, "The id should not be null");
+    if (id < 1) {
+      throw new IllegalArgumentException("The id should be superior to 0 !");
+    }
+    this.id = id;
   }
 
+  /**
+   * Returns the number of commits done.
+   *
+   * @return {@link Long}, the number of commits done.
+   */
   public Long getTotalCommits() {
     return totalCommits;
   }
 
+  /**
+   * Sets the number of commits.<br>
+   * The commits should not be null nor inferior to 0.
+   *
+   * @param totalCommits {@link Long}, the new number of commits.
+   */
   public void setTotalCommits(Long totalCommits) {
-    this.totalCommits = Objects.requireNonNull(totalCommits);
+    Objects.requireNonNull(totalCommits, "The number of commits should not be null !");
+    if (totalCommits < 0) {
+      throw new IllegalArgumentException("The number of commits should not be inferior to 0");
+    }
+    this.totalCommits = totalCommits;
   }
-  
+
+  /**
+   * Returns the number of tags.
+   *
+   * @return {@link Long}
+   */
   public Long getTotalTags() {
     return totalTags;
   }
 
+  /**
+   * Sets the number of tags.<br>
+   * The parameter should not be null.
+   *
+   * @param totalTags {@link Long}, the new number of tags.
+   */
   public void setTotalTags(Long totalTags) {
     this.totalTags = Objects.requireNonNull(totalTags);
   }
   
+  /**
+   * Returns the number of files for every programming language.
+   *
+   * @return {@link Map}<{@link String}, {@link Long}>,
+   *        the current files count for every language.
+   */
   public Map<String, Long> getLanguageAndFileCount() {
     return languageAndFileCount;
   }
 
+  /**
+   * Sets the files count for every programming language.<br>
+   * The parameter should not be null.
+   *
+   * @param languageAndFileCount {@link Map}<{@link String}, {@link Long}>,
+   *        the new files count for every language.
+   */
   public void setLanguageAndFileCount(Map<String, Long> languageAndFileCount) {
     this.languageAndFileCount = Objects.requireNonNull(languageAndFileCount);
   }
   
+  /**
+   * Returns the lines/language ratio.
+   *
+   * @return {@link Map}<{@link String}, {@link Long}>,
+   *          the lines/language ratio.
+   */
   public Map<String, Double> getLanguageRatio() {
     return languageRatio;
   }
 
+  /**
+   * Sets the lines/language ratio.<br>
+   * The parameter should not be null.
+   *
+   * @param languageRatio {@link Map}<{@link String}, {@link Long}>,
+   *        the new lines/language ratio.
+   */
   public void setLanguageRatio(Map<String, Double> languageRatio) {
     this.languageRatio = Objects.requireNonNull(languageRatio);
   }
   
+  /**
+   * Returns the number of branches.
+   *
+   * @return {@link Long}, the number of branches.
+   */
   public Long getTotalBranches() {
     return totalBranches;
   }
 
+  /**
+   * Sets the number of branches.<br>
+   * The parameter should not be null.
+   *
+   * @param totalBranches {@link Long}, the new number of branches.
+   */
   public void setTotalBranches(Long totalBranches) {
     this.totalBranches = Objects.requireNonNull(totalBranches);
   }
 
+  /**
+   * Returns the git's owner.
+   *
+   * @return {@link String}, the owner.
+   * @deprecated The owner attribute is wrong, it should not be used.
+   */
   public String getOwner() {
     return owner;
   }
 
+  /**
+   * Sets the owner.<br>
+   * The parameter should not be null.
+   *
+   * @param owner {@link String}, the new owner.
+   * @deprecated The owner attribute is wrong, it should not be used.
+   */
   public void setOwner(String owner) {
     this.owner = Objects.requireNonNull(owner);
   }
